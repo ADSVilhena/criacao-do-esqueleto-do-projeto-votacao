9906c6c9e21d8c0a5416b9f6a109a542f10808ed
JeanSchnorr <jeanschnorr2000@gmail.com>
2019-04-23 21:32:56 -0400

    exibindo alunos e votacoes em andamento e votacoes concluidas

--- SistemaTCC/votacoes/views.py
+++ SistemaTCC/votacoes/views.py
@@ -7,22 +7,26 @@ from .models import Aluno, Votacao
 @login_required
 def index(request):
     return render(request,'index.html',{'nomeMenu':'Início'})
+
 @login_required
 def listarAlunos(request):
     alunos = Aluno.objects.all()
     return render(request,'listar/listarAlunos.html',{'alunos':alunos})
+
 @login_required
 def emAndamento(request):
     context = {}
     votacoesEmAndamento = Votacao.objects.filter(concluida=False)
     context['votacoesEmAndamento'] = votacoesEmAndamento
     return render(request,'votacoes/emAndamento.html',context)
+    
 @login_required
 def concluidas(request):
     context = {}
     votacoesConcluidas = Votacao.objects.filter(concluida=True)
     context['votacoesConcluidas'] = votacoesConcluidas
     return render(request,'votacoes/concluidas.html',context)
+    
 @login_required
 def voto(request,cpfAluno):
     aluno = Aluno.objects.filter(cpfAluno=cpfAluno)
